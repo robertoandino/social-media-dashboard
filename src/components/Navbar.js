5178e0fb07b4f43af1e857c0d5d41448eff38c7e
Roberto Andino <andinorobert518@yahoo.com>
2024-11-18 15:24:18 -0500

    Styling.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
             <input
                 type="text"
                 placeholder="Search..."
-                className="p-2 rounded border bg-blue-400 text-white placeholder-white border-gray-300 focus:outline-none"
+                className="p-2 rounded border bg-gray-800 text-white placeholder-white border-gray-300 focus:outline-none"
             />
             <div className="flex items-center space-x-4">
                 <span className="text-white">John Doe</span>
