926a7c1a563d1ee39a09b82308f78810069971ed
Roberto Andino <andinorobert518@yahoo.com>
2024-11-20 18:57:16 -0500

    Styling.

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -25,8 +25,10 @@ function ProfileCard({ user }) {
         <div className="relative bg-gray-700 text-white p-6 rounded-lg shadow-lg max-w-sm">
             {/* Profile */}
             <div className="flex items-center space-x-4">
-                <div className="p-0.5 rounded-full bg-gradient-to-r
-                                from-yellow-500 via-orange-500 to-red-500">
+                <div className={`p-0.5 rounded-full bg-gradient-to-r 
+                                ${user.color === "red" ? "from-yellow-500 via-orange-500 to-red-500" : 
+                                user.color === "purple" ?  "from-purple-500 via-indigo-400 to-indigo-400" :
+                                "from-yellow-500 via-yellow-400 to-green-300"}`}>
                     <div className="border-2 border-gray-700 rounded-full p-3 bg-gray-700">
                         <img
                             src={user.avatar}
