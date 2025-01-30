5381334d087559da9aa0f2029b114f9e82923bbf
Roberto Andino <andinorobert518@yahoo.com>
2025-01-30 16:44:34 -0500

    Working on ProfilePage.

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function ProfilePage() {
     return(
-        <div>
-            <h1>Profile Page</h1>
+        <div className="flex justify-center items-center h-screen">
+            <div className="relative bg-gray-700 text-white p-6 rounded-lg shadow-lg 
+                            w-full md:w-96 lg:w-120" 
+            >
+                <h1 className="text-2xl font-bold">Profile Page</h1>
+                <p className="mt-4">Content</p>
+            </div>
         </div>
     )
 }
