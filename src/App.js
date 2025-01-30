27c9a89b109755ee50218fc2c90336acda1c0041
Roberto Andino <andinorobert518@yahoo.com>
2025-01-29 19:00:12 -0500

    Adding routes to sidebar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardLayout from './components/DashboardLayout';
 import Navbar from './components/Navbar';
 import Feed from './components/Feed';
@@ -114,22 +115,41 @@ function App() {
 };
 
   return (
-    <DashboardLayout>
-      <Navbar />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 p-6">
-        {/**Left Column*/}
-        <div className="space-y-6">
-          <ProfileCard user={selectedUser}/>
-          <AIPanel/>
-        </div>
+    <Router>
+      <DashboardLayout>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 p-6">
+                {/**Left Column*/}
+                <div className="space-y-6">
+                  <ProfileCard user={selectedUser}/>
+                  <AIPanel/>
+                </div>
 
-        {/**Right Column*/}
-        <div className="lg:col-span-2 space-y-6">
-          <CreatePost user={selectedUser} onAnimatePost={handlePostAnimation} />
-          <Feed posts={posts} selectedUser={selectedUser} onUserClick={setSelectedUser} animatedPost={animatedPost} onUpdatedPosts={handleUpdatedPosts} likesCounter={likesCounter}/>
-        </div>
-      </div>
-    </DashboardLayout>
+                {/**Right Column*/}
+                <div className="lg:col-span-2 space-y-6">
+                  <CreatePost user={selectedUser} onAnimatePost={handlePostAnimation} />
+                  <Feed 
+                    posts={posts} 
+                    selectedUser={selectedUser} 
+                    onUserClick={setSelectedUser} 
+                    animatedPost={animatedPost} 
+                    onUpdatedPosts={handleUpdatedPosts} 
+                    likesCounter={likesCounter}
+                  />
+                </div>
+              </div>
+            }
+          />
+          <Route path="/profile" element={<div>Profile Page</div>} />
+          <Route path="/AI" element={<div>AI Page</div>} />
+          <Route path="/settings" element={<div>Settings Page</div>} />
+        </Routes>
+      </DashboardLayout>
+    </Router>
   );
 }
 
